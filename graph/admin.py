8e19d2cf7fe8e039cfa1f6f64ee7a4498919e731
Inseong Hwang <insung151@naver.com>
2018-07-21 20:16:31 +0900

    어드민 페이지

--- graph/admin.py
+++ graph/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
-
+from django.contrib.auth import models
 from .models import *
 
 
+class ContribAdmin(admin.ModelAdmin):
+    list_display = ('member', 'level', 'date', )
+    list_per_page = 50
+    list_filter = ['member']
+
+
 admin.site.register(Member)
-admin.site.register(Contrib)
+admin.site.register(Contrib, ContribAdmin)
