3869e20f9908105e1d547ae63aeb3ed526b78ce9
Inseong Hwang <insung151@naver.com>
2018-07-21 20:15:40 +0900

    필드 이름 변경

--- graph/models.py
+++ graph/models.py
@@ -1,11 +1,24 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
 
 
 class Member(models.Model):
-    git_user_name = models.CharField(max_length=100,)
+    github_username = models.CharField(max_length=100, unique=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = self.github_username
+        super(Member, self).save(*args, **kwargs)
+
+    def get_latest_contribs(self):
+        return list(map(
+            lambda x: x.level,
+            self.contrib_set.filter(date__gte=datetime.date.today()-datetime.timedelta(7))
+        ))
 
     def __str__(self):
-        return str(self.git_user_name)
+        return str(self.github_username)
 
 
 class Contrib(models.Model):
@@ -14,5 +27,5 @@ class Contrib(models.Model):
     level = models.PositiveIntegerField()
 
     def __str__(self):
-        return str(self.member.git_user_name) + " / " + str(self.date) \
-               + " / " +str(self.level)
+        return str(self.member.github_username) + " / " + str(self.date) \
+               + " / " + str(self.level)
