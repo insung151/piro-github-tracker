d0ee415b7e41d6dc05de64df4105466d37ce1aa5
Inseong Hwang <insung151@naver.com>
2018-07-28 01:49:36 +0900

    로그 저장 모델 생성

--- graph/models.py
+++ graph/models.py
@@ -3,6 +3,8 @@ import datetime
 from django.db import models
 from django.utils import timezone
 
+# from .backends import update_data
+
 
 class Member(models.Model):
     github_username = models.CharField(max_length=100, unique=True)
@@ -17,6 +19,10 @@ class Member(models.Model):
             self.contrib_set.filter(date__gte=datetime.date.today()-datetime.timedelta(7))
         ))
 
+    def process(self):
+        if not self.logs.filter(created_at__gt=timezone.now() - datetime.timedelta(hours=3)).exists():
+            CrawlLog.objects.create(member=self).process()
+
     def __str__(self):
         return str(self.github_username)
 
@@ -29,3 +35,15 @@ class Contrib(models.Model):
     def __str__(self):
         return str(self.member.github_username) + " / " + str(self.date) \
                + " / " + str(self.level)
+
+
+class CrawlLog(models.Model):
+    member = models.ForeignKey(Member, related_name="logs")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def process(self):
+        # update_data(self.member)
+        pass
+
+    def __str__(self):
+        return str(self.created_at)
