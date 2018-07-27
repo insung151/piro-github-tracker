5e8c23bf280af30069f61d38c8db568e71462149
Inseong Hwang <insung151@naver.com>
2018-07-28 01:48:51 +0900

    member admin customizing

--- graph/admin.py
+++ graph/admin.py
@@ -4,9 +4,17 @@ from .models import *
 
 
 class ContribAdmin(admin.ModelAdmin):
-    list_display = ('member', 'level', 'date', )
+    list_display = ('member', 'level', 'date',)
     list_per_page = 50
     list_filter = ['member']
+
+
+class MemberAdmin(admin.ModelAdmin):
+    list_display = ('github_username', 'updated_at')
+
+    def updated_at(self, member):
+        if __name__ == '__main__':
+            return str(member.logs.objects.last())
 
 
 admin.site.register(Member)
